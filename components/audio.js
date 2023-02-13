397e175c6e45baa33ea9cf888e548b2952848494
snury <yuri.snisar@gmail.com>
2023-02-13 09:51:59 +0200

    change player styles

--- components/audio.js
+++ components/audio.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import styles from './audio.module.css';
 
 export default function Audio(props) {
   return (
-    <div className='Audio'>
-      <AudioPlayer
-    autoPlay
+    <div className={styles.container} >
+    <AudioPlayer
     src='https://yolopipe.com/wp-content/uploads/2022/06/Ed-Sheeran-Perfect.mp3'
     onPlay={e => console.log("onPlay")}
     // other props here
