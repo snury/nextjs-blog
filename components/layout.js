552dbaf88fc059a21887ab26d09e89097745a370
snury <yuri.snisar@gmail.com>
2023-02-13 09:41:29 +0200

    fix font link

--- components/layout.js
+++ components/layout.js
@@ -26,7 +26,7 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@500&display=swap" rel="stylesheet" .>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@500&display=swap" rel="stylesheet" />
       </Head>
       <header className={styles.header}>
         {home ? (
